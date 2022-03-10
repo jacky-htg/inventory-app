20f85cf89c1b3e178ba9ba2a91a4929ccb7acb37
Rijal Asepnugroho <rizal@dedomena.com.sg>
2022-03-11 06:17:29 +0700

    1. fix search general 2. fix stock location 3. fix items list

--- src/screens/Items/index.jsx
+++ src/screens/Items/index.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Item } from '../../services';
+import { Item, Lov } from '../../services';
 import PageList from '../../components/PageList';
 
 function Page() {
   const [fields, setFields] = useState([]);
+  const [sourcesData, setSourcesData] = useState([]);
+  const [sourcesOpt, setSourcesOpt] = useState([]);
 
   const setupFields = () => {
     setFields([
@@ -12,7 +14,7 @@ function Page() {
       { label: 'Description', field: 'description', default: true, filter: true, sorter: true },
       { label: 'QOH', field: 'qoh', default: true, filter: true, sorter: true },
       { label: 'Loc', field: 'loc', default: true, filter: true, sorter: true },
-      { label: 'Source', field: 'source', default: true, filter: true, sorter: true },
+      { label: 'Source', field: 'source', default: true, filter: true, sorter: true, lov: sourcesData, lookup: sourcesOpt },
       { label: 'Manufacturer', field: 'manufacturer', default: true, filter: true, sorter: true },
       { label: 'Product Group', field: 'productGroup', default: true, filter: true, sorter: true },
       { label: 'Category Code', field: 'categoryCode', default: true, filter: true, sorter: true },
@@ -24,6 +26,22 @@ function Page() {
 
   useEffect(() => {
     setupFields();
+  }, [sourcesData, sourcesOpt]);
+
+  useEffect(() => {
+    console.log("source opt >> ", sourcesOpt);
+  }, [sourcesOpt]);
+
+  useEffect(() => {
+    const data = Lov.getSources();
+    let temp = [];
+    data.then(res => {
+      res.forEach(e => {
+        temp.push(<Option key={ e.codeValue } value={ e.codeValue } >{ e.codeDesc }</Option>);  
+      });
+      setSourcesOpt(temp);
+      setSourcesData(res);
+    });
   }, []);
 
   return (
